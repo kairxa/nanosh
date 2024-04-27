956ad178cbbb354ea908e31932d8a3155af4e3f2
kairxa <kairxa@gmail.com>
2024-04-28 00:12:14 +0700

    Fix: Add Savant calculation to analyze action

--- src/calculator/actions/bridge/command/analyze.test.ts
+++ src/calculator/actions/bridge/command/analyze.test.ts
@@ -8,9 +8,25 @@ import { describe, expect, it } from 'bun:test'
 import analyze from './analyze'
 
 describe('action.bridge.command.analyze', () => {
-  const [gameState, _] = GetInitialGame('initial-state-game-test', 1715619600000)
+  const [gameState, _] = GetInitialGame(
+    'initial-state-game-test',
+    1715619600000,
+  )
   const solasMercer = gameState?.characters.get('Solas Mercer')
   solasMercer!.ap = 7
+  const niralPierce = gameState?.characters.get('Niral Pierce')
+  niralPierce!.ap = 7
+  niralPierce?.modifiers.set('character.cycle.deprived', {
+    start: {
+      day: 1,
+      cycle: 1,
+    },
+    expiry: {
+      day: -1,
+      cycle: -1,
+    },
+    amount: 4,
+  })
   gameState!.intel.basic = 8
 
   it('should analyze properly', () => {
@@ -25,6 +41,26 @@ describe('action.bridge.command.analyze', () => {
     expect(error).toBeNull()
     expect(newState?.characters.get('Solas Mercer')?.ap).toBe(7)
     expect(newState?.intel.basic).toBe(1)
+    expect(newState?.intel.critical).toBe(1)
+  })
+
+  it('should calculate skill.savant', () => {
+    let newState: Game | null
+    let error: Error | null
+    ;[newState, error] = analyze({
+      invokeTime: 123,
+      characterID: 'Niral Pierce',
+      state: gameState!,
+    })
+
+    expect(error).toBeNull()
+    expect(newState?.characters.get('Niral Pierce')?.ap).toBe(6)
+    expect(
+      newState?.characters
+        .get('Niral Pierce')
+        ?.modifiers.get('character.cycle.deprived')?.amount,
+    ).toBe(1)
+    expect(newState?.intel.basic).toBe(2)
     expect(newState?.intel.critical).toBe(1)
   })
 
--- src/calculator/actions/bridge/command/analyze.ts
+++ src/calculator/actions/bridge/command/analyze.ts
@@ -12,6 +12,8 @@ import skillModifiers, { SKILL_AP_REDUCE } from './skillModifiers'
 const ANALYZE_AP_USAGE = 1
 const ANALYZE_BASIC_INTEL_USAGE = 7
 const ANALYZE_CRIT_INTEL_ADDITION = 1
+const ANALYZE_SAVANT_DEPRIVED_REDUCE = 3
+const ANALYZE_SAVANT_BASIC_INTEL_REDUCE = 1
 
 interface BridgeCommandAnalyzeParams
   extends Pick<
@@ -40,7 +42,23 @@ export default function ({
   )
   if (error !== null) return [null, error]
 
-  stateCopy.intel.basic -= ANALYZE_BASIC_INTEL_USAGE
+  let basicIntelUsage = ANALYZE_BASIC_INTEL_USAGE
+  if (character.skills.has('skill.savant')) {
+    const currentDeprived = character.modifiers.get('character.cycle.deprived')
+    character.modifiers.set('character.cycle.deprived', {
+      start: {
+        day: currentDeprived?.start.day || 1,
+        cycle: currentDeprived?.start.cycle || 1,
+      },
+      expiry: {
+        day: currentDeprived?.expiry.day || -1,
+        cycle: currentDeprived?.expiry.cycle || -1,
+      },
+      amount: (currentDeprived?.amount || 0) - ANALYZE_SAVANT_DEPRIVED_REDUCE,
+    })
+    basicIntelUsage -= ANALYZE_SAVANT_BASIC_INTEL_REDUCE
+  }
+  stateCopy.intel.basic -= basicIntelUsage
   stateCopy.intel.critical += ANALYZE_CRIT_INTEL_ADDITION
   character.cycleActions.set(invokeTime, 'action.bridge.command.analyze')
   character.ap -= apUsed
