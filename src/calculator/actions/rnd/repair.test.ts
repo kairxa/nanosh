cae72ba60a474d503dd62751c26007e3cf49e475
kairxa <kairxa@gmail.com>
2024-05-08 22:50:28 +0700

    Feat: Add Repair action

---
+++ src/calculator/actions/rnd/repair.test.ts
@@ -0,0 +1,129 @@
+import {
+  INVALID_INVENTORY_ITEM_NOT_FOUND,
+  INVALID_REPAIR_ITEM_IS_NOT_BROKEN,
+} from '@nanosh/messages/errors'
+import type { Game } from '@nanosh/types/game'
+import { GetInitialGame } from '@nanosh/utils/initialState/game'
+import { describe, expect, it } from 'bun:test'
+import repair from './repair'
+
+describe('action.rnd.repair', () => {
+  const [gameState, _] = GetInitialGame('repair-test')
+  gameState!.characters.get('Alisa Huang')!.ap = 7
+  gameState!.characters
+    .get('Alisa Huang')!
+    .modifiers.set('character.cycle.deprived', {
+      start: { day: 1, cycle: 1 },
+      expiry: { day: -1, cycle: -1 },
+      amount: 7,
+    })
+  gameState!.characters.get('Alisa Huang')!.inventory.add({
+    id: 'alisa-huang-item-01',
+    itemName: 'body.heavy.lorica',
+    broken: true,
+  })
+  gameState!.characters.get('Alisa Huang')!.inventory.add({
+    id: 'alisa-huang-item-02',
+    itemName: 'weapon.heavy.arcus-driver',
+    broken: false,
+  })
+  gameState!.characters.get('Soren Koda')!.ap = 7
+  gameState!.characters
+    .get('Soren Koda')!
+    .modifiers.set('character.cycle.deprived', {
+      start: { day: 1, cycle: 1 },
+      expiry: { day: -1, cycle: -1 },
+      amount: 7,
+    })
+  gameState!.characters.get('Soren Koda')!.inventory.add({
+    id: 'soren-koda-item-01',
+    itemName: 'body.heavy.lorica',
+    broken: true,
+  })
+
+  it('should repair item and reduce deprived', () => {
+    let newState: Game | null
+    let error: Error | null
+    ;[newState, error] = repair({
+      state: gameState!,
+      itemID: 'soren-koda-item-01',
+      invokeTime: 123,
+      characterID: 'Soren Koda',
+    })
+
+    expect(error).toBeNull()
+    expect(
+      newState?.characters.get('Soren Koda')?.inventory.values().next().value,
+    ).toMatchObject({
+      id: 'soren-koda-item-01',
+      itemName: 'body.heavy.lorica',
+      broken: false,
+    })
+    expect(
+      newState?.characters
+        .get('Soren Koda')
+        ?.modifiers.get('character.cycle.deprived')?.amount,
+    ).toBe(6)
+    expect(newState?.characters.get('Soren Koda')?.ap).toBe(6)
+    expect(newState?.characters.get('Soren Koda')?.cycleActions.get(123)).toBe(
+      'action.rnd.repair',
+    )
+  })
+
+  it('should repair item and does not reduce deprived', () => {
+    let newState: Game | null
+    let error: Error | null
+    ;[newState, error] = repair({
+      state: gameState!,
+      itemName: 'body.heavy.lorica',
+      invokeTime: 123,
+      characterID: 'Alisa Huang',
+    })
+
+    expect(error).toBeNull()
+    expect(
+      newState?.characters.get('Alisa Huang')?.inventory.values().next().value,
+    ).toMatchObject({
+      id: 'alisa-huang-item-01',
+      itemName: 'body.heavy.lorica',
+      broken: false,
+    })
+    expect(
+      newState?.characters
+        .get('Alisa Huang')
+        ?.modifiers.get('character.cycle.deprived')?.amount,
+    ).toBe(7)
+    expect(newState?.characters.get('Alisa Huang')?.ap).toBe(5)
+    expect(newState?.characters.get('Alisa Huang')?.cycleActions.get(123)).toBe(
+      'action.rnd.repair',
+    )
+  })
+
+  it('should invalidate request due to item not in inventory', () => {
+    let newState: Game | null
+    let error: Error | null
+    ;[newState, error] = repair({
+      state: gameState!,
+      itemName: 'acc.voxlink',
+      invokeTime: 123,
+      characterID: 'Alisa Huang',
+    })
+
+    expect(newState).toBeNull()
+    expect(error?.message).toBe(INVALID_INVENTORY_ITEM_NOT_FOUND)
+  })
+
+  it('should invalidate request because item is not broken', () => {
+    let newState: Game | null
+    let error: Error | null
+    ;[newState, error] = repair({
+      state: gameState!,
+      itemName: 'weapon.heavy.arcus-driver',
+      invokeTime: 123,
+      characterID: 'Alisa Huang',
+    })
+
+    expect(newState).toBeNull()
+    expect(error?.message).toBe(INVALID_REPAIR_ITEM_IS_NOT_BROKEN)
+  })
+})
---
+++ src/calculator/actions/rnd/repair.ts
@@ -0,0 +1,63 @@
+import {
+  INVALID_INVENTORY_ITEM_NOT_FOUND,
+  INVALID_REPAIR_ITEM_IS_NOT_BROKEN,
+} from '@nanosh/messages/errors'
+import type {
+  DefaultCalculatorReturnType,
+  GenericCalculatorParams,
+} from '@nanosh/types/generic'
+import type { ItemNames } from '@nanosh/types/item'
+import type { Skills } from '@nanosh/types/skills'
+import getAPUsage from '@nanosh/utils/getAPUsage'
+import dutifulDeprivedReduce from '../modifiers/traits/dutiful'
+
+interface RndRepairParams
+  extends Pick<
+    GenericCalculatorParams,
+    'state' | 'invokeTime' | 'characterID'
+  > {
+  itemName?: ItemNames
+  itemID?: string
+}
+
+const REPAIR_AP_USAGE = 2
+const REPAIR_SKILL_MODIFIER = new Set<Skills>([
+  'skill.adaptable',
+  'skill.technician',
+])
+const REPAIR_SKILL_MODIFIER_AP_REDUCE = 1
+
+export default function ({
+  state,
+  characterID,
+  invokeTime,
+  itemName,
+  itemID,
+}: RndRepairParams): DefaultCalculatorReturnType {
+  const stateCopy = structuredClone(state)
+
+  const character = stateCopy.characters.get(characterID)
+  const inventory = Array.from(character!.inventory.values())
+  const item = inventory.find((item) => {
+    return item.itemName === itemName || item.id === itemID
+  })
+  if (!item) return [null, new Error(INVALID_INVENTORY_ITEM_NOT_FOUND)]
+
+  if (!item.broken) return [null, new Error(INVALID_REPAIR_ITEM_IS_NOT_BROKEN)]
+
+  const [apUsed, error] = getAPUsage(
+    character!,
+    REPAIR_SKILL_MODIFIER,
+    REPAIR_AP_USAGE,
+    REPAIR_SKILL_MODIFIER_AP_REDUCE,
+  )
+  if (error !== null) return [null, error]
+
+  item.broken = false
+
+  character!.cycleActions.set(invokeTime, 'action.rnd.repair')
+  character!.ap -= apUsed
+  const [newState, _] = dutifulDeprivedReduce({ state: stateCopy, characterID })
+
+  return [newState, null]
+}
